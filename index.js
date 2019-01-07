3ce43e8d01820a927278399c82359cba4c485ade
Jeremiah  Acayturri <jtacayturri@gmail.com>
2019-01-07 02:04:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(Math.random()*100) + 1;
+ var shopCart = {"itemName": item,"itemPrice": price};
+ cart.push(shopCart);
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty.";
+  }
 }
 
 function total() {
